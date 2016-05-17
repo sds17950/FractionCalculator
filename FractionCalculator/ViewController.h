bf1f8de30f48f6a3c73aa4968e23b8c6bc5fd311
MF839-001 <mf839-001@70-12-240-159.pools.spcsdns.net>
2016-05-17 10:34:13 +0900

    first commit

---
+++ FractionCalculator/ViewController.h
@@ -0,0 +1,31 @@
+//
+//  ViewController.h
+//  FractionCalculator
+//
+//  Created by MF839-001 on 2016. 5. 16..
+//  Copyright © 2016년 MF839-001. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+- (IBAction)clickDigit:(id)sender;
+
+- (IBAction)clickOver:(id)sender;
+- (IBAction)clickClear:(id)sender;
+- (IBAction)clickEquals:(id)sender;
+
+- (IBAction)clickMinus:(id)sender;
+- (IBAction)clickPlus:(id)sender;
+- (IBAction)clickMultiply:(id)sender;
+- (IBAction)clickDivide:(id)sender;
+
+- (void)processDigit:(int)digit;
+- (void)processOp:(char)theOp;
+- (void)storeFracPart;
+
+@end
+
